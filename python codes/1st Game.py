dfb087968673c4f9332701aef2dac546cfc4475f
Shovan Biswas <shovanbiswas2004@gmail.com>
2023-06-15 23:29:09 +0530

    game complete

--- python codes/1st Game.py
+++ python codes/1st Game.py
@@ -43,9 +43,10 @@ def handle_bullets(first_bullet,sec_bullet,first_ship,sec_ship):
         if first_ship.colliderect(bullet):
             pygame.event.post(pygame.event.Event(first_hit))
             sec_bullet.remove(bullet)
-            
-        elif bullet.x <0:
+       
+        elif bullet.x <0 :
             sec_bullet.remove(bullet)
+            
 def draw_window(first_bullet,sec_bullet, first_health, sec_health ):
     for bullet in sec_bullet:
         pygame.draw.rect(screen,'red',bullet)
@@ -54,6 +55,7 @@ def draw_window(first_bullet,sec_bullet, first_health, sec_health ):
 
 text = font.render('Please press space to start',True,'white')
 textm= text.get_rect(midbottom = (440,460))
+count= 0
 
 background= pygame.image.load(os.path.join('python codes', 'files', 'background.webp'))
 background_new = pygame.transform.scale(background,(900,600))
@@ -70,13 +72,14 @@ while True:
             pygame.quit()
             exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LCTRL and len(first_bullet)< max_bullet :
+            if event.key == pygame.K_SPACE and len(first_bullet)< max_bullet :
                 bullet = pygame.Rect(first_ship.x + first_ship.width , first_ship.y + first_ship.height//2-2,10,5)
                 first_bullet.append(bullet)
 
                 
             if event.key == pygame.K_RCTRL and len(sec_bullet)< max_bullet:
                 bullet = pygame.Rect(sec_ship.x , sec_ship.y + sec_ship.height//2-2,10,5)
+               
                 sec_bullet.append(bullet)
                 
         if event.type == sec_hit:
@@ -92,7 +95,11 @@ while True:
 
     if sec_health<=0:
         winner = "The FORCE won"
-    if winner!="":
+    if winner!="":  
+        if count <900:
+            count+=900
+            pygame.draw.rect(screen,'cornflowerblue',(0,0,count, 600))
+
         draw_winner(winner)
         break
 
@@ -105,6 +112,7 @@ while True:
     pygame.draw.rect(screen,'black',border)
     screen.blit(background_new,(0,0))
     screen.blit(text,textm)
+   
     screen.blit(ship_new,first_ship)
     screen.blit(tie_new,sec_ship)
     enter = pygame.key.get_pressed()
