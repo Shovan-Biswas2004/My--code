108343ac3a1cd97412df562608a0c96a41087175
Shovan Biswas <shovanbiswas2004@gmail.com>
2023-06-16 15:06:17 +0530

    all complete working on asthetics

--- python codes/1st Game.py
+++ python codes/1st Game.py
@@ -1,26 +1,35 @@
-import pygame
+#importhing the modules :
+import pygame 
 import os
 from sys import exit
 pygame.font.init()
 pygame.init()
+
+# display :
 screen = pygame.display.set_mode((900,600))
 pygame.display.set_caption('STAR WARS-PvP-')
 CLOCK = pygame.time.Clock()
-no = 0
+
+# SURFACE commands :
 border= pygame.Rect(449, 0, 1, 600)
 surface = pygame.Surface((900,600))
 surface.fill('white')
+
+# Fonts :
 font = pygame.font.Font(None,40)
 health_font = pygame.font.SysFont('comicsans', 40)
 winner_font = pygame.font.SysFont('Bauhaus 93', 40)
+
+#Bullets :
 first_bullet =[]
 sec_bullet =[]
 max_bullet = 3
+bullet= 0
 first_hit = pygame.USEREVENT +1
 sec_hit = pygame.USEREVENT +2
 first_health = 10
 sec_health = 10
-bullet= 0
+no = 0
 def draw_winner(name):
     winner_text = winner_font.render(name, 1, 'white')
     screen.blit(winner_text,(900//2- winner_text.get_width()/2,600//2-winner_text.get_height()/2))
@@ -53,19 +62,24 @@ def draw_window(first_bullet,sec_bullet, first_health, sec_health ):
     for bullet in first_bullet:
         pygame.draw.rect(screen,'blue',bullet)
 
+#texts :
 text = font.render('Please press space to start',True,'white')
 textm= text.get_rect(midbottom = (440,460))
-count= 0
 
+# Rectangles :
 background= pygame.image.load(os.path.join('python codes', 'files', 'background.webp'))
 background_new = pygame.transform.scale(background,(900,600))
 ship= pygame.image.load(os.path.join('python codes', 'files', 'ship.png'))
 tie= pygame.image.load(os.path.join('python codes', 'files', 'tie.png'))
+heart =  pygame.image.load(os.path.join('python codes', 'files', 'heart.png'))
+heart_new = pygame.transform.scale(heart,(70,60))
+heart_n = pygame.transform.scale(heart,(70,60))
 ship_new= pygame.transform.scale(ship,(80,70))
 first_ship = ship_new.get_rect(midbottom = (200,200))
 tie_new= pygame.transform.rotate(pygame.transform.scale(tie,(150,90)), 270)
 sec_ship = tie_new.get_rect(midbottom = (700,500))
 
+count= 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -82,8 +96,8 @@ while True:
                
                 sec_bullet.append(bullet)
                 
-        if event.type == sec_hit:
-            sec_health-=1
+                if event.type == sec_hit:
+                    sec_health-=1
             
 
         if event.type == first_hit:
@@ -95,16 +109,9 @@ while True:
 
     if sec_health<=0:
         winner = "The FORCE won"
-    if winner!="":  
-        if count <900:
-            count+=900
-            pygame.draw.rect(screen,'cornflowerblue',(0,0,count, 600))
-
+    if winner!="":
         draw_winner(winner)
         break
-
-        
-         
 
 
     handle_bullets(first_bullet,sec_bullet,first_ship,sec_ship)
@@ -122,8 +129,10 @@ while True:
         
     if no==1:
         
-        score1 = health_font.render("Score: "+ str(first_health), False , 'white')
-        score2 = health_font.render("Score: "+ str(sec_health), False , 'white')
+        score1 = health_font.render("     : "+ str(first_health), False , 'white')
+        score2 = health_font.render("     : "+ str(sec_health), False , 'white')
+        screen.blit(heart_new ,(10,20))
+        screen.blit(heart_n,(900-score2.get_width()-20,20))
         screen.blit(score1,(20,20))
         screen.blit(score2,(900-score2.get_width()-10,20))
         textm.y+=5      
